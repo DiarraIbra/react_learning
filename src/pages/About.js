a6defa4c96784998966e3fbf54920f080152e94c
DiarraIbra <ibrahimadiarraibou@gmail.com>
2025-02-13 23:44:59 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1> A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure assumenda
+        repellat maxime quia, facere accusamus amet fugit consectetur
+        reprehenderit architecto debitis dignissimos laboriosam dicta, mollitia
+        ea cum. Error modi, molestias sit veritatis aliquid eligendi labore,
+        quis repudiandae placeat reiciendis ea ipsam exercitationem recusandae,
+        expedita provident vero laborum harum dolorum deleniti! Dignissimos
+        corporis eligendi dolore eveniet rerum quo commodi dolorum, sequi, culpa
+        nulla soluta ipsa voluptas ea voluptate optio recusandae reprehenderit
+        maxime ipsum rem totam dicta! Tempore eius porro amet facilis quisquam
+        adipisci deserunt reprehenderit dolorem odit. Optio nulla est eligendi,
+        atque eaque enim ad consequuntur hic rem aut necessitatibus alias.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas dolor
+        quis quo sapiente est perspiciatis, voluptatum ea praesentium. Tempore a
+        quia cumque amet ea. Vero tempora ex ipsum, dolore consequuntur
+        temporibus dicta. Voluptas atque facere blanditiis sed quis explicabo
+        velit dolorem culpa nulla voluptatibus natus ipsum saepe illo vel et,
+        quo numquam ipsam consequuntur eveniet. Praesentium, rem! Officia soluta
+        quam delectus unde quaerat? Excepturi nam voluptate sunt itaque
+        asperiores deserunt laborum qui sint debitis iste rem animi consequatur
+        eveniet possimus, iusto temporibus beatae, maxime, quam perspiciatis
+        tenetur perferendis consequuntur dicta nesciunt. Quod nostrum, minima
+        rerum illo ut, maiores repellendus debitis maxime suscipit minus quae
+        fugiat explicabo animi. Illum possimus quisquam, deserunt ullam
+        perspiciatis tempore dolor non neque tenetur, incidunt dicta, ut modi
+        quia aperiam porro quod repellat pariatur labore iusto commodi itaque
+        expedita similique. Et eos beatae nemo obcaecati eveniet aut consectetur
+        quas. Neque quos, dolorum iste explicabo obcaecati laborum vero
+        sapiente? Natus dolor animi nisi in adipisci, nostrum quos illo vitae
+        laudantium porro, quod quibusdam voluptatibus ad debitis obcaecati
+        cupiditate rerum dignissimos consequuntur ex accusamus corporis, unde
+        iusto. Voluptate beatae facilis nostrum modi ratione maxime dolorum,
+        itaque iure, deserunt aspernatur doloribus esse vel omnis, perspiciatis
+        alias obcaecati quis at!
+      </p>
+    </div>
+  );
+};
+
+export default About;
